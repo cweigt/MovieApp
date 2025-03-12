5bf0811741e35760f6b09693345b69122ae3620c
FootLettuce26 <caleb.weigt@cuw.edu>
2025-03-12 12:11:22 -0500

    starting the offical project, the other stuff was introduction

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-//everything that starts with the word 'use' is a hook
-import { useState } from 'react' 
-
-//card component allows for reusability
-//same as doing function Card(title){}
-const Card = ( { title } ) => { //accepting the title property (prop)
-  //a call to the useState hook
-  //having the useState here allows for seperate liking of each card
-  const [hasLiked, setHasLiked] = useState(false) //boolean variable, then a set function to update
-  
-  return (
-    <div className="card">
-      <h2>{title}</h2> {/*dynamically rendering title prop*/}
-
-      {/*Flipping it to enable the liking and unliking*/}
-      <button onClick={() => setHasLiked(!hasLiked)}> {/*settingHasLiked to the opposite state*/}
-        {hasLiked ? '🩷' : '🤍'} {/*setting display based on state*/}
-      </button>
-    </div>
-  )
-}
-
-//Thi is the main function that is called when the app is rendered
-//Each "Card component" below is an instance of the card component above
+import React from 'react';
+import Search from './components/Search';
 const App = () => {
   return (
-    <div className="card-container">
-      <Card title="Lord of the Rings" rating={5} isCool={true} />
-      <Card title="Avatar" />
-      <Card title="Star Wars" />  
-    </div>
-  )
-}
+    <main> 
 
-export default App
+      <div className="pattern"/> {/*Self closing div with classname*/}
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+
+          <h1>Find <span className="text-gradient">Movies</span> You'll Love</h1>
+        </header>
+
+        <Search /> {/*Search component*/}
+      </div>
+    </main>
+  );
+};
+
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 //PURPOSE: allows to customize the build process
 //plugins: array of plugins to use in the build process
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
