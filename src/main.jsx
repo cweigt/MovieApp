44f63a6768c2b57d5ecf5e48215ee86812f78d29
FootLettuce26 <caleb.weigt@cuw.edu>
2025-05-05 09:46:14 -0500

    Making this a PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.css'
 import App from './App.jsx'
+
+serviceWorkerRegistration.register(); //service worker for PWA
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
